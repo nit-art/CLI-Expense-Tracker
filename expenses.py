f2a90fa25e798a58ab2bd741f267514c3ca14d6c
Art <nitesh220604@gmail.com>
2026-05-29 19:35:02 +0530

    trying to make the class work somehow

---
+++ expenses.py
@@ -0,0 +1,5 @@
+class expense():
+    def __init__(self,category,expense):
+        self.category=category
+        self.expense=expense
+        print(f"category{category} expense{expense}")
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-class expense():
-    def __init__(self,category,expense):
-        self.category=category
-        self.expense=expense
+from expenses import *
+import sqlite3
+def get_int(integer):
+    while True:
+        try :
+            return int(input(integer))
+            
+        except:
+            print("please input a Integer Only")
+            continue
+choice=get_int("\nplease enter your choice \n1. Add expense \n2. View Expense\n3. Category\n4. total spent\n5. Exit\nchoice : ")
+
+while True:
+    if choice == 1:
+        pass
+    elif choice==2:
+        pass
+    elif choice==3:
+        pass
+    elif choice==4:
+        pass
+    elif choice==5:
+        print("Thank you for using the program")
+        break
